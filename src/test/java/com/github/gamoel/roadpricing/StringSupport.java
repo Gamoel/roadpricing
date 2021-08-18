3c98a7b79f4c3ecb0bbd3dd4b11f54dc8de7de70
Kerus <ritterh500@gmail.com>
2021-08-18 21:40:34 +0200

    Remove unused class

--- src/test/java/com/github/gamoel/roadpricing/StringSupport.java
+++
@@ -1,7 +0,0 @@
-package com.github.gamoel.roadpricing;
-
-public class StringSupport {
-    static String getTrailingCharacters(String string, int count) {
-        return string.substring(string.length() - count);
-    }
-}
