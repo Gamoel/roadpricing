018c9bdc51c846b76be229bb2221dd7ee527bc1b
Kerus <ritterh500@gmail.com>
2021-08-18 20:22:49 +0200

    Modify Assertion for toString to Matchers.startsWith

--- src/test/java/com/github/gamoel/roadpricing/VehicleTest.java
+++ src/test/java/com/github/gamoel/roadpricing/VehicleTest.java
@@ -1,6 +1,7 @@
 package com.github.gamoel.roadpricing;
 
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class VehicleTest {
@@ -11,13 +12,13 @@ public class VehicleTest {
     @Test
     public void toStringRepresentationOfVehicle_A() {
         Vehicle vehicle = getVehicleWith("BB-667Z", 320, 15785, 0, 182850);
-        Assertions.assertEquals("   BB-667Z 320kW 15785kg  0   1828.50 EUR", vehicle.toString());
+        MatcherAssert.assertThat(vehicle.toString(), Matchers.startsWith("   BB-667Z 320kW 15785kg  0   1828.50 EUR"));
     }
 
     @Test
     public void toStringRepresentationOfVehicle_B() {
         Vehicle vehicle = getVehicleWith("BP-133", 75, 210, 0, 3000);
-        Assertions.assertEquals("    BP-133  75kW   210kg  0     30.00 EUR", vehicle.toString());
+        MatcherAssert.assertThat(vehicle.toString(), Matchers.startsWith("    BP-133  75kW   210kg  0     30.00 EUR"));
     }
 
     private Vehicle getVehicleWith(final String registration, final int power, final int weight, final int passengers, final int toll) {
