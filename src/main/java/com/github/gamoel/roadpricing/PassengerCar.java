863bc03b17061464db5bd32fcf55ace537d1a357
Kerus <ritterh500@gmail.com>
2021-08-18 21:28:41 +0200

    Make PassengerCar extend Vehicle

--- src/main/java/com/github/gamoel/roadpricing/PassengerCar.java
+++ src/main/java/com/github/gamoel/roadpricing/PassengerCar.java
@@ -1,25 +1,23 @@
 package com.github.gamoel.roadpricing;
 
-public class PassengerCar {
+public class PassengerCar extends Vehicle {
     static final int BASETOLL = 5000;
     static final int PASSENGERTOLL = 2000;
     static final int POWERTOLLPERKW = 99;
     static final String IDENTIFIER = "[PassengerCar]";
-    private final int power;
-    private final int passenger;
 
 
-    public PassengerCar(int power, int passenger) {
-        this.power = power;
-        this.passenger = passenger;
+    public PassengerCar(String registration, int power, int weight, int passengers) {
+        super(registration, power, weight, passengers);
     }
 
+    @Override
     public int getToll() {
-        return BASETOLL + (this.passenger * PASSENGERTOLL) + (power * POWERTOLLPERKW);
+        return BASETOLL + (getPassengers() * PASSENGERTOLL) + (getPower() * POWERTOLLPERKW);
     }
 
     @Override
     public String toString() {
-        return IDENTIFIER;
+        return super.toString() + " " + IDENTIFIER;
     }
 }
--- src/test/java/com/github/gamoel/roadpricing/PassengerCarTest.java
+++ src/test/java/com/github/gamoel/roadpricing/PassengerCarTest.java
@@ -1,27 +1,30 @@
 package com.github.gamoel.roadpricing;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class PassengerCarTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.endsWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PassengerCarTest extends VehicleTest {
 
     @Test
     public void tollOfLoneRider() {
         PassengerCar passengerCar = unpoweredCarWithPassengerCount(0);
-        Assertions.assertEquals(PassengerCar.BASETOLL, passengerCar.getToll());
+        assertEquals(PassengerCar.BASETOLL, passengerCar.getToll());
     }
 
     @Test
     public void tollWithOnePassenger() {
         PassengerCar passengerCar = unpoweredCarWithPassengerCount(1);
-        Assertions.assertEquals(PassengerCar.BASETOLL +
+        assertEquals(PassengerCar.BASETOLL +
                 PassengerCar.PASSENGERTOLL, passengerCar.getToll());
     }
 
     @Test
     public void tollWithTwoPassenger() {
         PassengerCar passengerCar = unpoweredCarWithPassengerCount(2);
-        Assertions.assertEquals(PassengerCar.BASETOLL + (PassengerCar.PASSENGERTOLL * 2)
+        assertEquals(PassengerCar.BASETOLL + (PassengerCar.PASSENGERTOLL * 2)
                 , passengerCar.getToll());
     }
 
@@ -29,15 +32,14 @@ public class PassengerCarTest {
     public void tollIsAlsoBasedOnPower() {
         final int power = 100;
         PassengerCar passengerCar = loneDriverWithPower(power);
-        Assertions.assertEquals(PassengerCar.BASETOLL +
+        assertEquals(PassengerCar.BASETOLL +
                 (power * PassengerCar.POWERTOLLPERKW), passengerCar.getToll());
     }
 
     @Test
     public void toStringAppendsIdentifier() {
         String vehicleString = somePassengerCar().toString();
-        Assertions.assertEquals(PassengerCar.IDENTIFIER,
-                StringSupport.getTrailingCharacters(vehicleString, PassengerCar.IDENTIFIER.length()));
+        assertThat(vehicleString, endsWith(" " + PassengerCar.IDENTIFIER));
     }
 
     private PassengerCar somePassengerCar() {
@@ -45,11 +47,21 @@ public class PassengerCarTest {
     }
 
     private PassengerCar unpoweredCarWithPassengerCount(int passengerCount) {
-        return new PassengerCar(0, passengerCount);
+        return new PassengerCar("", 0, 0, passengerCount);
     }
 
 
     private PassengerCar loneDriverWithPower(int power) {
-        return new PassengerCar(power, 0);
+        return new PassengerCar("", power, 0, 0);
+    }
+
+    @Override
+    protected Vehicle getVehicleWith(String registration, int power, int weight, int passengers, int toll) {
+        return new PassengerCar(registration, power, weight, passengers) {
+            @Override
+            public int getToll() {
+                return toll;
+            }
+        };
     }
 }
