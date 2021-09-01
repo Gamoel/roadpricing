b6884180501ff01e3ce33f9f1e91473acbe483f6
Kerus <ritterh500@gmail.com>
2021-09-01 20:28:40 +0200

    Use Mocking Framework

--- src/test/java/com/github/gamoel/roadpricing/MainTest.java
+++ src/test/java/com/github/gamoel/roadpricing/MainTest.java
@@ -1,19 +1,10 @@
 package com.github.gamoel.roadpricing;
 
 import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import org.mockito.Mockito;
 
 class MainTest {
-    private String text = "";
-    private TextPrinter printer = new TextPrinter() {
-
-        @Override
-        public void printLine(String string) {
-            text = text + string + "\n";
-        }
-    };
+    private TextPrinter printer = Mockito.mock(TextPrinter.class);
 
     @Test
     public void printOneItemFromArray() {
@@ -22,7 +13,7 @@ class MainTest {
         };
 
         printTable(printer, vehicleArray);
-        assertThat(text, equalTo("   BB-667Z 320kW 15785kg  0   1828.50 EUR [Truck]\n"));
+        Mockito.verify(printer).printLine("   BB-667Z 320kW 15785kg  0   1828.50 EUR [Truck]");
     }
 
     @Test
@@ -33,10 +24,9 @@ class MainTest {
         };
 
         printTable(printer, vehicleArray);
-        assertThat(text, equalTo(
-                "" +
-                        "   BB-667Z 320kW 15785kg  0   1828.50 EUR [Truck]\n" +
-                        "    BP-133  75kW   210kg  0     30.00 EUR [MotorBike]\n"));
+        Mockito.verify(printer).printLine("   BB-667Z 320kW 15785kg  0   1828.50 EUR [Truck]");
+        Mockito.verify(printer).printLine("    BP-133  75kW   210kg  0     30.00 EUR [MotorBike]");
+
     }
 
     private void printTable(TextPrinter printer, Vehicle[] vehicleArray) {
